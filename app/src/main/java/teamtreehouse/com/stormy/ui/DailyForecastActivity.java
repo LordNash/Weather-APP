f2703f7e0ee8af46e6594dd52224358e9d5c89fd
LordNash <thegamebacha@gmail.com>
2015-07-30 12:10:20 +0100

    My message

---
+++ app/src/main/java/teamtreehouse/com/stormy/ui/DailyForecastActivity.java
@@ -0,0 +1,112 @@
+package teamtreehouse.com.stormy.ui;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Arrays;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import teamtreehouse.com.stormy.R;
+import teamtreehouse.com.stormy.adapters.DayAdapter;
+import teamtreehouse.com.stormy.weather.Day;
+
+public class DailyForecastActivity extends Activity {
+
+    private Day[] mDays;
+    @InjectView(android.R.id.list)
+    ListView mListView;
+    @InjectView(android.R.id.empty)
+    TextView mEmptyTextView ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_forecast);
+        ButterKnife.inject(this);
+
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.DAILY_FORECAST);
+        mDays = Arrays.copyOf(parcelables, parcelables.length, Day[].class);
+
+        DayAdapter adapter = new DayAdapter(this, mDays);
+        mListView.setAdapter(adapter);
+        mListView.setEmptyView(mEmptyTextView);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String dayOfTheWeek = mDays[i].getDayOfTheWeek();
+                String conditions = mDays[i].getSummary();
+                String hightTemp = mDays[i].getTemperatureMax() + "";
+                String message = String.format("On %s the high will be and it will be %s",
+                        dayOfTheWeek, hightTemp, conditions);
+                Toast.makeText(DailyForecastActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+}
+
+
+
+/*import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.Arrays;
+
+import teamtreehouse.com.stormy.R;
+import teamtreehouse.com.stormy.adapters.DayAdapter;
+import teamtreehouse.com.stormy.weather.Day;
+
+public class DailyForecastActivity extends ListActivity {
+
+    private Day[] mDays;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_forecast);
+
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.DAILY_FORECAST);
+        mDays = Arrays.copyOf(parcelables, parcelables.length, Day[].class);
+
+        DayAdapter adapter = new DayAdapter(this, mDays);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String dayOfTheWeek = mDays[position].getDayOfTheWeek();
+        String conditions = mDays[position].getSummary();
+        String hightTemp = mDays[position].getTemperatureMax()+"";
+        String message = String.format("On %s the high will be and it will be %s",
+                dayOfTheWeek,hightTemp,conditions);
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+}*/
+
+
+
+
+
+
+
+
+
